20db5eae217645782fd708d5676efbfda6bfe02f
Alex <alexnjoroge102@gmail.com>
2025-03-02 14:27:03 +0300

    removed navlink

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -15,7 +15,7 @@ import user6 from "../assets/profile-pictures/user6.jpg";
 export const navItems = [
   { label: "Home", href: "#" },
   { label: "About", href: "#" },
-  { label: "Workflow", href: "#" },
+  
   
 ];
 
